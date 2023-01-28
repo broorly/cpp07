8338e1561028a38de9ce05a042214fd882d9f34c
Mohamed-amine Rafik <mrafik@e2r3p1.1337.ma>
2023-01-28 21:53:38 +0100

    all good

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mrafik <mrafik@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/27 21:41:48 by mrafik            #+#    #+#             */
-/*   Updated: 2023/01/28 17:19:14 by mrafik           ###   ########.fr       */
+/*   Updated: 2023/01/28 19:13:46 by mrafik           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
 std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
 std::string c = "chaine1";
 std::string d = "chaine2";
-::swap(c, d);
+swap(c, d);
 std::cout << "c = " << c << ", d = " << d << std::endl;
 std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
 std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
